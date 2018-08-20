893b37a01763c5fcea7a15fd138492f709b9332b
Aliza Miller <alizamiller1@gmail.com>
2018-08-19 23:32:07 -0500

    Updated Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,17 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hour lourem ipsem.......";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Author: Humberto Ramos",
+                "Inks: Victor Olazabo"
+            };
+
             return View();
         }
     }
